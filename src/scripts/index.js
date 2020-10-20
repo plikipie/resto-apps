8fd5edca85c4efb8b2a72366bd26a71001aa27da
plikipe <plikipie@gmail.com>
2020-10-20 23:05:32 +0700

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/css/main.css";
+import "../styles/css/appbar.css";
+import "../styles/css/jumbotron.css";
+import "../styles/css/content.css";
+console.log("Hello Coders! :)");
