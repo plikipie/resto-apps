dc41b6f04c58bf4b0316cbf23c7400e873750b56
plikipe <plikipie@gmail.com>
2020-10-23 16:52:55 +0700

    Menambahkan kontent first content

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -6,8 +6,10 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("slide").style.backgroundColor = "#2E8BC0";
     document.getElementById("slide").style.opacity = "1";
+    document.getElementById("slide").style.transition = "0.5s";
   } else {
     document.getElementById("slide").style.backgroundColor = "";
     document.getElementById("slide").style.opacity = "";
+    document.getElementById("slide").style.transition = "0.5s";
   }
 }
