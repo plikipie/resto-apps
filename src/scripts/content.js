79666540897659899b4a2d17acb07fb24c68cb53
plikipe <plikipie@gmail.com>
2020-10-30 23:24:31 +0700

    added aria label on logo + meta description submission accepted

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -4,7 +4,7 @@ let getData = "";
 data.restaurants.forEach((resto) => {
   getData += `<div tabindex="0" class="main__card">
     <h1 class="img__tagline">Kota: ${resto.city}</h1>
-    <img src="${resto.pictureId}" alt="" class="card__img" />
+    <img src="${resto.pictureId}" alt="${resto.name}" class="card__img" />
     <div class="card__tagline">
         <h2 class="rating">Rating : ${resto.rating}</h2>
         <h3 class="img__title">${resto.name}</h3>
